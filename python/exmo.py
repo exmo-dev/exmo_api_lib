632c78cd60b2418329dc2b222dc6d8e78ea171b9
ignatov357 <kosnaum@mail.ru>
2017-06-09 20:25:39 +0300

    API library for python updated

--- python/exmo.py
+++ python/exmo.py
@@ -1,30 +1,46 @@
-import httplib
+import http.client
 import urllib
 import json
 import hashlib
 import hmac
 import time
- 
-api_key = "your_key"
-api_secret = "your_secret"
 
-nonce = int(round(time.time()*1000))
- 
-params = {"nonce": nonce}
-params = urllib.urlencode(params)
- 
-H = hmac.new(api_secret, digestmod=hashlib.sha512)
-H.update(params)
-sign = H.hexdigest()
- 
-headers = {"Content-type": "application/x-www-form-urlencoded",
-           "Key":api_key,
-           "Sign":sign}
-conn = httplib.HTTPSConnection("api.exmo.com")
-conn.request("POST", "/v1/user_info", params, headers)
-response = conn.getresponse()
- 
-print response.status, response.reason
-print json.load(response)
- 
-conn.close()
+class ExmoAPI:
+    def __init__(self, API_KEY, API_SECRET, API_URL = 'api.exmo.com', API_VERSION = 'v1'):
+        self.API_URL = API_URL
+        self.API_VERSION = API_VERSION
+        self.API_KEY = API_KEY
+        self.API_SECRET = bytes(API_SECRET, encoding='utf-8')
+
+    def sha512(self, data):
+        H = hmac.new(key = self.API_SECRET, digestmod = hashlib.sha512)
+        H.update(data.encode('utf-8'))
+        return H.hexdigest()
+
+    def api_query(self, api_method, http_method = "POST", params = {}):
+        params['nonce'] = int(round(time.time() * 1000))
+        params =  urllib.parse.urlencode(params)
+        
+        sign = self.sha512(params)
+        headers = {
+            "Content-type": "application/x-www-form-urlencoded",
+            "Key": self.API_KEY,
+            "Sign": sign
+        }
+        conn = http.client.HTTPSConnection(self.API_URL)
+        conn.request(http_method, "/" + self.API_VERSION + "/" + api_method, params, headers)
+        response = conn.getresponse().read()
+        
+        conn.close()
+
+        try:
+            obj = json.loads(response.decode('utf-8'))
+            if 'error' in obj and obj['error']:
+                raise ScriptError(obj['error'])
+            return obj
+        except json.decoder.JSONDecodeError:
+            raise ScriptError('An error occurred, while parsing response string:', response)
+
+# Example
+ExmoAPI_instance = ExmoAPI('YOUR API KEY', 'YOUR API SECRET')
+print(ExmoAPI_instance.api_query('user_info'))
