bdd5f57918b5b85cb19682c6da5d87fa6c07bd0c
Vadim Inshakov <vadiminshakov@gmail.com>
2019-06-24 00:22:15 +0300

    refactor to simple example of lib usage

---
+++ golang/example.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/exmo-dev/exmo_api_lib/tree/master/golang/lib"
+)
+
+func main() {
+
+	key := ""    // TODO replace with your api key from profile page
+	secret := "" // TODO replace with your api secret from profile page
+
+	result, err := lib.Api_query(key, secret, "user_info", nil)
+	if err != nil {
+		fmt.Printf("api error: %s\n", err.Error())
+	} else {
+		fmt.Println("api result:")
+		for key, value := range result {
+			if key == "balances" {
+				fmt.Println("\n-- balances:")
+				for k, v := range value.(map[string]interface{}) {
+					fmt.Println(k, v)
+				}
+			}
+			if key == "reserved" {
+				fmt.Println("\n-- reserved:")
+				for k, v := range value.(map[string]interface{}) {
+					fmt.Println(k, v)
+				}
+			}
+		}
+
+	}
+
+	fmt.Printf("-------------\n")
+
+	usertrades, err1 := lib.Api_query(key, secret, "user_trades", lib.ApiParams{"pair": "BTC_RUB"})
+	if err1 != nil {
+		fmt.Printf("api error: %s\n", err1.Error())
+	} else {
+		fmt.Println("User trades")
+		for pair, val := range usertrades {
+			fmt.Printf("\n\n %s", pair)
+			for _, interfacevalue := range val.([]interface{}) {
+				fmt.Printf("\n\n***\n")
+				for k, v := range interfacevalue.(map[string]interface{}) {
+					fmt.Println(k, v)
+				}
+			}
+		}
+	}
+}
---
+++ golang/go.mod
@@ -0,0 +1,7 @@
+module example
+
+go 1.12
+
+replace github.com/exmo-dev/exmo_api_lib/tree/master/golang/lib => ./lib/
+
+require github.com/exmo-dev/exmo_api_lib/tree/master/golang/lib v0.0.0-00010101000000-000000000000
