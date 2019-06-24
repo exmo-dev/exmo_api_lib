181294978c6c07996cbec6a6e06d9a54a211170a
Vadim Inshakov <vadiminshakov@gmail.com>
2019-06-25 00:37:41 +0300

    import lib module in tests instead of duplicating code

--- golang/lib/go.mod
+++ golang/lib/go.mod
@@ -1,3 +1,7 @@
 module lib
 
 go 1.12
+
+replace github.com/exmo-dev/exmo_api_lib/tree/master/golang/lib => ./
+
+require github.com/exmo-dev/exmo_api_lib/tree/master/golang/lib v0.0.0-00010101000000-000000000000
--- golang/lib/lib_test.go
+++ golang/lib/lib_test.go
@@ -1,18 +1,9 @@
 package lib
 
 import (
-	"bytes"
-	"crypto/hmac"
-	"crypto/sha512"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strconv"
+	"github.com/exmo-dev/exmo_api_lib/tree/master/golang/lib"
 	"testing"
-	"time"
 )
 
 func TestApi_query(t *testing.T) {
@@ -21,72 +12,9 @@ func TestApi_query(t *testing.T) {
 	key := ""    // TODO replace with your api key from profile page
 	secret := "" // TODO replace with your api secret from profile page
 
-	type ApiResponse map[string]interface{}
-	type ApiParams map[string]string
-
-	nonce := func() string {
-		t.Helper()
-		return fmt.Sprintf("%d", time.Now().UnixNano())
-	}
-
-	Do_sign := func(message string, secret string) string {
-		t.Helper()
-		mac := hmac.New(sha512.New, []byte(secret))
-		mac.Write([]byte(message))
-		return fmt.Sprintf("%x", mac.Sum(nil))
-	}
-
-	Api_query := func(key string, secret string, method string, params ApiParams) (ApiResponse, error) {
-		t.Helper()
-		post_params := url.Values{}
-		post_params.Add("nonce", nonce())
-		if params != nil {
-			for key, value := range params {
-				post_params.Add(key, value)
-			}
-		}
-		post_content := post_params.Encode()
-
-		sign := Do_sign(post_content, secret)
-
-		req, _ := http.NewRequest("POST", "https://api.exmo.com/v1/"+method, bytes.NewBuffer([]byte(post_content)))
-		req.Header.Set("Key", key)
-		req.Header.Set("Sign", sign)
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("Content-Length", strconv.Itoa(len(post_content)))
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		if resp.Status != "200 OK" {
-			return nil, errors.New("http status: " + resp.Status)
-		}
-
-		body, err1 := ioutil.ReadAll(resp.Body)
-		if err1 != nil {
-			return nil, err1
-		}
-
-		var dat map[string]interface{}
-		err2 := json.Unmarshal([]byte(body), &dat)
-		if err2 != nil {
-			return nil, err2
-		}
-
-		if result, ok := dat["result"]; ok && result.(bool) != true {
-			return nil, errors.New(dat["error"].(string))
-		}
-
-		return dat, nil
-	}
-
 	t.Run("Get user info", func(t *testing.T) {
 		fmt.Printf("-------------\n")
-		result, err := Api_query(key, secret, "user_info", nil)
+		result, err := lib.Api_query(key, secret, "user_info", nil)
 		if err != nil {
 			fmt.Printf("api error: %s\n", err.Error())
 		} else {
@@ -112,7 +40,7 @@ func TestApi_query(t *testing.T) {
 	t.Run("Get user trades", func(t *testing.T) {
 		fmt.Printf("-------------\n")
 
-		usertrades, err1 := Api_query(key, secret, "user_trades", ApiParams{"pair": "BTC_RUB"})
+		usertrades, err1 := lib.Api_query(key, secret, "user_trades", lib.ApiParams{"pair": "BTC_RUB"})
 		if err1 != nil {
 			fmt.Printf("api error: %s\n", err1.Error())
 		} else {
